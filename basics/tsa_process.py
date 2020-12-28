d3fbb43af7fbf8042df44209e7f06344fff54a5e
Jim <30752307+DJimmie@users.noreply.github.com>
2020-12-28 16:25:19 -0600

    lots of work review tsa basics. Decomposed tsa data using statsmodels.tsa.seasonal.STL.

--- basics/tsa_process.py
+++ basics/tsa_process.py
@@ -2,8 +2,38 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import calendar
 import os
+import sys
+import logging
+
+from pylab import rcParams
+import statsmodels.api as sm
+rcParams['figure.figsize'] = 11, 9
+
+from statsmodels.tsa.seasonal import STL
+from pandas.plotting import register_matplotlib_converters
+
+sys.path.insert(0,"C:\\Users\\dowdj\\OneDrive\\Documents\\GitHub\\my-modules-and-libraries\\program_work_dir")  # Temporary. Used to help finish development of modules.
+import program_work_dir as pwd
+
+register_matplotlib_converters()
+# sns.set_style('darkgrid')
+
+plt.rc('figure',figsize=(16,12))
+plt.rc('font',size=13)
+
+# %%
+# Create program working folder and its subfolders
+config_parameters={'database server':{'sqlite_server':'N/A'}}
+client=pwd.ClientFolder(os.path.basename(__file__),config_parameters)
+ini_file=f'c:/my_python_programs/{client}/{client}.ini'
+
+log_file=f'c:/my_python_programs/{client}/{client}_log.log'
+logging.basicConfig(filename=log_file, level=logging.INFO, filemode='w', format=' %(asctime)s -%(levelname)s - %(message)s')
+logging.info('Start')
+
 
 # %%
 # ------> get the time series data; setting the Datetime as Index
@@ -28,12 +58,10 @@ class RawData():
 
         self.subset=self.df.loc[start:stop, cols]
 
-        # self.s=DataPlot(self.subset)
-
-        print(self.subset.head())
-        print(type(self.subset))
-
-        return self.subset
+        subset=pd.DataFrame(self.subset)
+        print(subset.head())
+        
+        return subset
 
     def resample(self,freq):
 
@@ -48,43 +76,45 @@ class RawData():
 class DataPlot():
     """Visuallizations the various Time Series plots"""
 
-    def __init__(self,df,show=True):
+    def __init__(self,df,show=False):
         self.df=df
         self.show=show
         # self.plot1()
 
     def plot1(self):
 
-        self.df.plot(subplots=True, layout=None, figsize=(10, 8), sharex=True)
+        self.df.plot(subplots=True, layout=None, figsize=(15, 10), sharex=True)
         plt.grid()
 
 
         if self.show:
             plt.show()
 
-    def shift_plot(self,col,shift=1):
+    def shift_plot(self,col,shift=1,yscale='linear'):
 
         fig, ax = plt.subplots(nrows=2,ncols=1,sharex=True,figsize=(10, 6))
         fig.suptitle(f'Shift Plot: {col.upper()}', fontsize=16)
         ax[0].set_title(f'{col}')
+        ax[0].set_yscale(yscale)
         ax[0].plot(self.df[col], color='black')
         ax[0].grid()
         shift_name=f'{col}_shift_{shift}'
         shift_value = self.df[col].div(self.df[col].shift(shift))
         ax[1].set_title(f'{col}--->shift({shift})')
+        ax[1].set_yscale(yscale)
         ax[1].plot(shift_value, color='coral')
         ax[1].grid()
 
         if self.show:
             plt.show()
 
-    def pct_change_plot(self,col,shift=1):
+    def pct_change_plot(self,col,shift=1,yscale='linear'):
 
         fig, ax = plt.subplots(nrows=2,ncols=1,sharex=True,figsize=(10, 6))
         fig.suptitle(f'Percent Change Plot: {col.upper()}', fontsize=16)
         ax[0].set_title(f'{col}')
 
-        # ax[0].set_yscale('log')
+        ax[0].set_yscale(yscale)
 
         ax[0].plot(self.df.loc[:, col], color='black')
         ax[0].grid()
@@ -93,9 +123,9 @@ class DataPlot():
         pct = self.df[col].pct_change(shift)*100
         ax[1].set_title(f'{col}--->percent change({shift})')
 
-        # ax[1].set_yscale('log')
-
-        ax[1].plot(pct, color='red')
+        ax[1].set_yscale(yscale)
+        ax[1].bar(pct.index,pct, color='red')
+        # ax[1].plot(pct, color='red')
         ax[1].grid()
 
         if self.show:
@@ -127,6 +157,7 @@ class DataPlot():
         ax=self.df[col].expanding().mean().plot(label=f'{col} expanding mean')
         ax=self.df[col].expanding().median().plot(label=f'{col} expanding median')
         ax=self.df[col].expanding().std().plot(label=f'{col} expanding std')
+        ax=self.df[col].expanding().sum().plot(label=f'{col} expanding sum')
 
         ax.legend()
         plt.grid()
@@ -134,44 +165,116 @@ class DataPlot():
         if self.show:
             plt.show()
         
+    def rolling_window(self,col,period=1):
+        fig, ax = plt.subplots(nrows=1,ncols=1,sharex=False,figsize=(10, 6))
+        fig.suptitle(f'Rolling Window: {col.upper()}', fontsize=16)
+        ax.set_title(f'{col}')
+        ax=self.df[col].plot(label=f'{col}')
+        ax=self.df[col].rolling(period,center=True).mean().plot(label=f'{col} {period} Moving Avg')
+        plt.legend()
+        plt.grid()
+        
+        if self.show:
+            plt.show()
+
+
+class TSAnalysis():
+    """TS Analysis"""
+
+    def __init__(self,df):
+        self.df=df
+
+    def decompose (self,col):
+        # decomposition = sm.tsa.seasonal_decompose(pd.DataFrame(self.df[col]), model='Additive')
+        # fig = decomposition.plot()
+        a=self.df[col]
+        b=a.index
+        c=pd.infer_freq(b)
+        
+        d = pd.Series(a, index=pd.date_range(b.date[0], b.date[len(a)-1], periods=None, freq=c), name = col)
+        
+        print(d)
+        # x=pd.Series(p.values,index=p.index.values)
+        stl = STL(d, seasonal=7)
+        res = stl.fit()
+        fig = res.plot()
+        plt.show()
 
 
 # %%
-apha=RawData(datafile='APHA.csv',index_col='Date')
+
+
+## FUNCTIONS----------------------------------FUNCTIONS----------------------------------FUNCTIONS
+def get_config_values(section,option):
+    """Used to retrieve values from the program's configuration file."""
+    config=pwd.configparser.ConfigParser()
+    config.read(ini_file)
+
+    return config[section][option]
+
+
+def exit_operations():
+    """Operations to perform prior to the program exit"""
+    # remove images from the image folder after each session
+
+    # save the table as a json file in the working directory
+    pass
+
+
+## MAIN----------------------------------MAIN----------------------------------MAIN
+# %%
+
+data="quicken_dsv.csv"   #"APHA.csv"
+
+data_file=f'c:/my_python_programs/{client}/{data}'
+
+# apha=RawData(datafile=data_file,index_col='postedOn')
 
 # %%
-apha_monthly=apha.resample('M')
+bank=RawData(datafile=data_file,index_col='postedOn')
+# %%
+bank
+# %%
+bank.df
+# %%
+bank.df.index# %%
+bank.df.info()
+# %%
+bank_amount=bank.data_sub_set('2019','2020','amount')
+bank_amount.sort_values(by=['postedOn'],ascending=True)
+# %%
+bank_amount_plots=DataPlot(df=bank_amount)
+
+# %%
+bank_amount_sum=bank_amount.groupby('postedOn').sum()
+# %%
+bank_amount_sum_plots=DataPlot(df=bank_amount_sum)
+# bank_amount_sum_plots.plot1()
+# bank_amount_sum_plots.shift_plot('amount')
+# bank_amount_sum_plots.pct_change_plot('amount',yscale='log')
+# bank_amount_sum_plots.expanding_window('amount')
+# bank_amount_sum.plot(kind='bar')
 
 # %%
 
-apha_monthly
+# %%
+bank_amount_weekly=bank.resample(freq='W')
 
 # %%
-apha_monthly_plots=DataPlot(df=apha_monthly)
+bank_amount_weekly_plots=DataPlot(df=bank_amount_weekly)
 # %%
-apha_monthly_plots.plot1()
 # %%
-apha_plots=DataPlot(df=apha.df)
+bank_amount_weekly_plots.expanding_window('amount')
 # %%
-apha_plots.plot1()
+
+
+
+bank_amount_weekly_analysis=TSAnalysis(df=bank_amount_weekly)
+
 # %%
-apha_weekly=apha.resample('W')
+bank_amount_weekly_analysis.decompose('amount')
+
+
 # %%
-apha_weekly.head()
-# %%
-apha_weekly_plots=DataPlot(df=apha_weekly)
-# %%
-apha_weekly_plots.plot1()
-# %%
-apha_monthly_plots.shift_plot('Open')
-# %%
-apha_year_end=apha.resample('A')
-# %%
-apha_year_end.head()
-# %%
-apha_year_end_plots=DataPlot(df=apha_year_end)
-# %%
-apha_year_end_plots.plot1()
-# %%
-apha.rs
+plt.show()
 # %%
