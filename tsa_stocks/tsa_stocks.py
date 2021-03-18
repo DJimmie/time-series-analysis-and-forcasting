07c7b229d3b33e6fedaf586e935fdb82de3b72f3
Jim <30752307+DJimmie@users.noreply.github.com>
2021-03-17 19:11:14 -0500

    revised the input_parameters dict.

--- tsa_stocks/tsa_stocks.py
+++ tsa_stocks/tsa_stocks.py
@@ -261,12 +261,23 @@ if __name__ == '__main__':
 
     #-->Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
     # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    # input_parameters={
+    #     'source':'random_walk',
+    #     'symbol':'cron',
+    #     'start':None,
+    #     'end':None,
+    #     'feature':'data',
+    #     'interval':'1d',
+    #     'period':'2y'
+    #     }
+
+
     input_parameters={
-        'source':'random_walk',
+        'source':'stocks',
         'symbol':'cron',
         'start':None,
         'end':None,
-        'feature':'data',
+        'feature':'close',
         'interval':'1d',
         'period':'2y'
         }
